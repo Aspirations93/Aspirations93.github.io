d2afa5cdba24cfcd0d5935095cce6adc4ad8c055
Aspirations93 <anthonyestpierre@gmail.com>
2019-06-15 13:24:30 -0700

    json test

--- main.js
+++ main.js
@@ -2,12 +2,17 @@
 var request = new XMLHttpRequest()
 
 // Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://api.openbrewerydb.org/breweries', true)
+request.open('GET', 'https://api.openbrewerydb.org/breweries?by_state=california', true)
 
 request.onload = function () {
-  // Begin accessing JSON data here
-  console.log("Yes")
-  }
+    // Begin accessing JSON data here
+    var data = JSON.parse(this.response)
+
+    data.forEach(obj => {
+    // Log each movie's title
+    console.log(obj.city)
+    })
+}
 
 // Send request
 request.send()
